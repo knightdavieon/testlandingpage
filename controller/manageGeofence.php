245a9869079544361589df93e2466a6ca3a1bb5f
knightdavieon <espinosa.johndave27@gmail.com>
2024-09-04 14:35:16 +0800

    added authentication function

--- controller/manageGeofence.php
+++ controller/manageGeofence.php
@@ -1,22 +1,5 @@
 <?php
-$host = '3.26.76.229';
-$db = 'geofencing_db';
-$user = 'localphp';
-$pass = 'password';
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
+include('../db/connection.php');
 
 $method = $_SERVER['REQUEST_METHOD'];
 
