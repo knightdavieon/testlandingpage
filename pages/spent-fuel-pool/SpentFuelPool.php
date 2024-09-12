81c4192b9761c022af93b06509b24c9a9c892484
renald-001 <fabrorenald@gmail.com>
2024-09-12 23:58:23 +0800

    additional section

--- pages/spent-fuel-pool/SpentFuelPool.php
+++ pages/spent-fuel-pool/SpentFuelPool.php
@@ -19,6 +19,23 @@
         <link href="../../css/styles.css" rel="stylesheet" />
         <link href="../../css/stylesNars.css" rel="stylesheet" />
         <link href="../../css/stylesrnld.css" rel="stylesheet" />
+        <style>
+          .icon-1x {
+              font-size: 24px !important;
+          }
+          a{
+              text-decoration:none;    
+          }
+          .text-primary, a.text-primary:focus, a.text-primary:hover {
+              color: #00ADBB!important;
+          }
+          .text-black, .text-hover-black:hover {
+              color: #000 !important;
+          }
+          .font-weight-bold {
+              font-weight: 700 !important;
+          }
+        </style>
     </head>
     <body id="page-top">
         <!-- Header -->
@@ -66,43 +83,172 @@
                 </div>
             </div>
         </header>
-        <!-- About -->
+        <!-- Sharing Hub -->
         <section class="page-section bg-info" id="About">
-            <div class="container px-4 px-lg-5">
-                <h2 class="text-center text-dark mt-0">About</h2>
-                <hr class="divider" />
-                <div class="row gx-4 gx-lg-5">
-                    <p class="text-dark-75 mb-4">In a nuclear facility, each room is designed for specific functions and requires various tools to support safe and efficient operations. The tools used in these rooms are often specialized to handle radioactive materials, ensure precision in maintenance and repairs, and maintain safety protocols. The <strong>reactor hall</strong> in a nuclear power plant or facility is a large, secure space that houses the nuclear reactor and other associated equipment. Overall, the reactor hall is a critical part of the infrastructure that ensures the safe and efficient operation of a nuclear reactor. The primary purposes of a reactor hall include:</p>
-                    <div class="col-lg-3 col-md-6 text-center">
-                        <div class="mt-5">
-                            <div class="mb-2"><i class="bi-house fs-1 text-dark"></i></div>
-                            <h3 class="h4 mb-2">Housing the Reactor</h3>
-                            <p class="text-secondary mb-0">The reactor hall contains the reactor vessel, where the nuclear fission process occurs. This area is designed to provide both physical and radiation protection for personnel and the environment.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 text-center">
-                        <div class="mt-5">
-                            <div class="mb-2"><i class="bi-wrench fs-1 text-dark"></i></div>
-                            <h3 class="h4 mb-2">Maintenance and Refueling</h3>
-                            <p class="text-muted mb-0">The reactor hall allows for access to the reactor for maintenance and refueling operations. It is typically equipped with heavy-duty cranes and other equipment necessary for lifting and replacing fuel assemblies and reactor components.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 text-center">
-                        <div class="mt-5">
-                            <div class="mb-2"><i class="bi-shield fs-1 text-dark"></i></div>
-                            <h3 class="h4 mb-2">Radiation Shielding</h3>
-                            <p class="text-muted mb-0">The reactor hall is often designed with thick walls made of concrete or other materials that provide shielding from the radiation produced by the reactor. This helps protect workers and the surrounding environment.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 text-center">
-                        <div class="mt-5">
-                            <div class="mb-2"><i class="bi-clipboard-data fs-1 text-dark"></i></div>
-                            <h3 class="h4 mb-2">Control and Monitoring</h3>
-                            <p class="text-muted mb-0">Some areas within or adjacent to the reactor hall are used for monitoring and controlling reactor operations. While the primary control room is usually separate, auxiliary control stations or monitoring equipment may be present within the hall.</p>
-                        </div>
-                    </div>
-                </div>
+        <div class="container">
+      <div class="row">
+        <!-- Main content -->
+        <div class="col-lg-9 mb-3">
+          <div class="row text-left mb-5">
+            <div class="col-lg-6 mb-3 mb-sm-0">
+              <div class="dropdown bootstrap-select form-control form-control-lg bg-white bg-op-9 text-sm w-lg-50" style="width: 100%;">
+              <select class="form-control form-control-lg bg-white bg-op-9 text-sm w-lg-50" data-toggle="select" tabindex="-98">
+                <option> Categories </option>
+                <option> Learn </option>
+                <option> Share </option>
+                <option> Build </option>
+              </select>
+              </div>
             </div>
+            <div class="col-lg-6 text-lg-right">
+              <div class="dropdown bootstrap-select form-control form-control-lg bg-white bg-op-9 ml-auto text-sm w-lg-50" style="width: 100%;">
+                  <select class="form-control form-control-lg bg-white bg-op-9 ml-auto text-sm w-lg-50" data-toggle="select" tabindex="-98">
+                    <option> Filter by </option>
+                    <option> Votes </option>
+                    <option> Replys </option>
+                    <option> Views </option>
+                  </select>
+              </div>
+            </div>
+          </div>
+          <!-- End of post 1 -->
+          <div class="card row-hover pos-relative py-3 px-3 mb-3 border-warning border-top-0 border-right-0 border-bottom-0 rounded-0">
+            <div class="row align-items-center">
+              <div class="col-md-8 mb-3 mb-sm-0">
+                <h5>
+                  <a href="#" class="text-primary">Drupal 8 quick starter guide</a>
+                </h5>
+                <p class="text-sm"><span class="op-6">Posted</span> <a class="text-black" href="#">20 minutes</a> <span class="op-6">ago by</span> <a class="text-black" href="#">KenyeW</a></p>
+                <div class="text-sm op-5"> <a class="text-black mr-2" href="#">#C++</a> <a class="text-black mr-2" href="#">#AppStrap Theme</a> <a class="text-black mr-2" href="#">#Wordpress</a> </div>
+              </div>
+              <div class="col-md-4 op-7">
+                <div class="row text-center op-7">
+                  <div class="col px-1"> <i class="ion-connection-bars icon-1x"></i> <span class="d-block text-sm">141 Votes</span> </div>
+                  <div class="col px-1"> <i class="ion-ios-chatboxes-outline icon-1x"></i> <span class="d-block text-sm">122 Replys</span> </div>
+                  <div class="col px-1"> <i class="ion-ios-eye-outline icon-1x"></i> <span class="d-block text-sm">290 Views</span> </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /End of post 1 -->
+          <!-- End of post 2 -->
+          <div class="card row-hover pos-relative py-3 px-3 mb-3 border-primary border-top-0 border-right-0 border-bottom-0 rounded-0">
+            <div class="row align-items-center">
+              <div class="col-md-8 mb-3 mb-sm-0">
+                <h5>
+                  <a href="#" class="text-primary">HELP! My Windows XP machine is down</a>
+                </h5>
+                <p class="text-sm"><span class="op-6">Posted</span> <a class="text-black" href="#">54 minutes</a> <span class="op-6">ago by</span> <a class="text-black" href="#">DanielD</a></p>
+                <div class="text-sm op-5"> <a class="text-black mr-2" href="#">#Development</a> <a class="text-black mr-2" href="#">#AppStrap Theme</a> </div>
+              </div>
+              <div class="col-md-4 op-7">
+                <div class="row text-center op-7">
+                  <div class="col px-1"> <i class="ion-connection-bars icon-1x"></i> <span class="d-block text-sm">256 Votes</span> </div>
+                  <div class="col px-1"> <i class="ion-ios-chatboxes-outline icon-1x"></i> <span class="d-block text-sm">251 Replys</span> </div>
+                  <div class="col px-1"> <i class="ion-ios-eye-outline icon-1x"></i> <span class="d-block text-sm">223 Views</span> </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /End of post 2 -->
+          <!-- End of post 3 -->
+          <div class="card row-hover pos-relative py-3 px-3 mb-3 border-primary border-top-0 border-right-0 border-bottom-0 rounded-0">
+            <div class="row align-items-center">
+              <div class="col-md-8 mb-3 mb-sm-0">
+                <h5>
+                  <a href="#" class="text-primary">Bootstrap 4 development in record time with AppStrap Bootstrap 4 Theme</a>
+                </h5>
+                <p class="text-sm"><span class="op-6">Posted</span> <a class="text-black" href="#">32 minutes</a> <span class="op-6">ago by</span> <a class="text-black" href="#">AppStrapMaster</a></p>
+                <div class="text-sm op-5"> <a class="text-black mr-2" href="#">#Bootstrap 4</a> <a class="text-black mr-2" href="#">#Wordpress</a> </div>
+              </div>
+              <div class="col-md-4 op-7">
+                <div class="row text-center op-7">
+                  <div class="col px-1"> <i class="ion-connection-bars icon-1x"></i> <span class="d-block text-sm">245 Votes</span> </div>
+                  <div class="col px-1"> <i class="ion-ios-chatboxes-outline icon-1x"></i> <span class="d-block text-sm">116 Replys</span> </div>
+                  <div class="col px-1"> <i class="ion-ios-eye-outline icon-1x"></i> <span class="d-block text-sm">257 Views</span> </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /End of post 3 -->
+          <!-- End of post 4 -->
+          <div class="card row-hover pos-relative py-3 px-3 mb-3 border-warning border-top-0 border-right-0 border-bottom-0 rounded-0">
+            <div class="row align-items-center">
+              <div class="col-md-8 mb-3 mb-sm-0">
+                <h5>
+                  <a href="#" class="text-primary">Bootstrap 4 development in record time with AppStrap Bootstrap 4 Theme</a>
+                </h5>
+                <p class="text-sm"><span class="op-6">Posted</span> <a class="text-black" href="#">29 minutes</a> <span class="op-6">ago by</span> <a class="text-black" href="#">Themelize.me</a></p>
+                <div class="text-sm op-5"> <a class="text-black mr-2" href="#">#Android</a> <a class="text-black mr-2" href="#">#Bootstrap 4</a> <a class="text-black mr-2" href="#">#Wordpress</a> <a class="text-black mr-2" href="#">#Drupal</a> </div>
+              </div>
+              <div class="col-md-4 op-7">
+                <div class="row text-center op-7">
+                  <div class="col px-1"> <i class="ion-connection-bars icon-1x"></i> <span class="d-block text-sm">49 Votes</span> </div>
+                  <div class="col px-1"> <i class="ion-ios-chatboxes-outline icon-1x"></i> <span class="d-block text-sm">29 Replys</span> </div>
+                  <div class="col px-1"> <i class="ion-ios-eye-outline icon-1x"></i> <span class="d-block text-sm">170 Views</span> </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /End of post 4 -->
+        
+        </div>
+        <!-- Sidebar content -->
+        <div class="col-lg-3 mb-4 mb-lg-0 px-lg-0 mt-lg-0">
+          <div style="visibility: hidden; display: none; width: 285px; height: 801px; margin: 0px; float: none; position: static; inset: 85px auto auto;"></div><div data-settings="{&quot;parent&quot;:&quot;#content&quot;,&quot;mind&quot;:&quot;#header&quot;,&quot;top&quot;:10,&quot;breakpoint&quot;:992}" data-toggle="sticky" class="sticky" style="top: 85px;"><div class="sticky-inner">
+            <a class="btn btn-lg btn-block btn-success rounded-0 py-4 mb-3 bg-op-6 roboto-bold" href="#">Ask Question</a>
+            <div class="bg-white mb-3">
+              <h4 class="px-3 py-4 op-5 m-0">
+                Active Topics
+              </h4>
+              <hr class="m-0">
+              <div class="pos-relative px-3 py-3">
+                <h6 class="text-primary text-sm">
+                  <a href="#" class="text-primary">Why Bootstrap 4 is so awesome? </a>
+                </h6>
+                <p class="mb-0 text-sm"><span class="op-6">Posted</span> <a class="text-black" href="#">39 minutes</a> <span class="op-6">ago by</span> <a class="text-black" href="#">AppStrapMaster</a></p>
+              </div>
+              <hr class="m-0">
+              <div class="pos-relative px-3 py-3">
+                <h6 class="text-primary text-sm">
+                  <a href="#" class="text-primary">Custom shortcut or command to launch command in terminal? </a>
+                </h6>
+                <p class="mb-0 text-sm"><span class="op-6">Posted</span> <a class="text-black" href="#">58 minutes</a> <span class="op-6">ago by</span> <a class="text-black" href="#">DanielD</a></p>
+              </div>
+              <hr class="m-0">
+              <div class="pos-relative px-3 py-3">
+                <h6 class="text-primary text-sm">
+                  <a href="#" class="text-primary">HELP! My Windows XP machine is down </a>
+                </h6>
+                <p class="mb-0 text-sm"><span class="op-6">Posted</span> <a class="text-black" href="#">48 minutes</a> <span class="op-6">ago by</span> <a class="text-black" href="#">DanielD</a></p>
+              </div>
+              <hr class="m-0">
+              <div class="pos-relative px-3 py-3">
+                <h6 class="text-primary text-sm">
+                  <a href="#" class="text-primary">HELP! My Windows XP machine is down </a>
+                </h6>
+                <p class="mb-0 text-sm"><span class="op-6">Posted</span> <a class="text-black" href="#">38 minutes</a> <span class="op-6">ago by</span> <a class="text-black" href="#">DanielD</a></p>
+              </div>
+              <hr class="m-0">
+            </div>
+            <div class="bg-white text-sm">
+              <h4 class="px-3 py-4 op-5 m-0 roboto-bold">
+                Stats
+              </h4>
+              <hr class="my-0">
+              <div class="row text-center d-flex flex-row op-7 mx-0">
+                <div class="col-sm-6 flex-ew text-center py-3 border-bottom border-right"> <a class="d-block lead font-weight-bold" href="#">58</a> Topics </div>
+                <div class="col-sm-6 col flex-ew text-center py-3 border-bottom mx-0"> <a class="d-block lead font-weight-bold" href="#">1.856</a> Posts </div>
+              </div>
+              <div class="row d-flex flex-row op-7">
+                <div class="col-sm-6 flex-ew text-center py-3 border-right mx-0"> <a class="d-block lead font-weight-bold" href="#">300</a> Members </div>
+                <div class="col-sm-6 flex-ew text-center py-3 mx-0"> <a class="d-block lead font-weight-bold" href="#">DanielD</a> Newest Member </div>
+              </div>
+            </div>
+          </div></div>
+        </div>
+      </div>
+    </div>
         </section>
         <!-- Trainings-->
         <section class="page-section bg-secondary text-black" id="training">
